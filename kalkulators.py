787bdde3f4cc0d036ec3cde23fa3f8a0b76340e7
Roberts15 <roberts.berzins25@gmail.com>
2022-02-25 14:22:29 +0200

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -1,5 +1,8 @@
 import numbers
+from math import*
 from tkinter import*
+from unittest.util import strclass
+
 def btnClick(number):
     current=e.get() #nolasa esošo skaitli
     e.delete(0,END) #nodzēš
@@ -17,18 +20,31 @@ def btnCommand(command):
     return 0
 
 def btnEquals():
-    num2=int(e.get())
+    num2=0
+    num2=float(e.get())
     result=0
-    if mathOp=="+":
-        result=num1+num2
-    elif mathOp=="-":
-        result=num1-num2
-    elif mathOp=="x":
-        result=num1*num2
-    elif mathOp=="/":
-        result=num1/num2
-    else:
-        result=0
+    if num2!=0:
+        if mathOp=="+":
+            result=num1+num2
+        elif mathOp=="-":
+            result=num1-num2
+        elif mathOp=="x":
+            result=num1*num2
+        elif mathOp=="/":
+            result=num1/num2
+        elif mathOp=="%":
+            result=(0.01*num1)*num2
+        elif mathOp=="_/":
+            result=num2**(1/num1)
+        else:
+            result=0
+    elif num2==0:
+        if mathOp=="%":
+            result=num1*0.01
+        elif mathOp=="_/":
+            result=num1**0.5
+        else:
+            result=0
     e.delete(0,END)
     e.insert(0,str(result))
     return 0 
@@ -42,26 +58,28 @@ def Clear():
 print("python strādā")
 mansLogs=Tk()
 mansLogs.title("Kalkulators")
-e=Entry(mansLogs,width=15,font=("Daytona",20))
-btn0=Button(mansLogs, text="0", padx="40", pady="20", command=lambda:btnClick(0))
-btn1=Button(mansLogs, text="1", padx="40", pady="20", command=lambda:btnClick(1))
-btn2=Button(mansLogs, text="2", padx="40", pady="20", command=lambda:btnClick(2))
-btn3=Button(mansLogs, text="3", padx="40", pady="20", command=lambda:btnClick(3))
-btn4=Button(mansLogs, text="4", padx="40", pady="20", command=lambda:btnClick(4))
-btn5=Button(mansLogs, text="5", padx="40", pady="20", command=lambda:btnClick(5))
-btn6=Button(mansLogs, text="6", padx="40", pady="20", command=lambda:btnClick(6))
-btn7=Button(mansLogs, text="7", padx="40", pady="20", command=lambda:btnClick(7))
-btn8=Button(mansLogs, text="8", padx="40", pady="20", command=lambda:btnClick(8))
-btn9=Button(mansLogs, text="9", padx="40", pady="20", command=lambda:btnClick(9))
+e=Entry(mansLogs,width=15, bd=20, font=("Daytona",20))
+btn0=Button(mansLogs, text="0", padx="40",bd=10 , pady="20", command=lambda:btnClick(0))
+btn1=Button(mansLogs, text="1", padx="40",bd=10 , pady="20", command=lambda:btnClick(1))
+btn2=Button(mansLogs, text="2", padx="40",bd=10 , pady="20", command=lambda:btnClick(2))
+btn3=Button(mansLogs, text="3", padx="40",bd=10 , pady="20", command=lambda:btnClick(3))
+btn4=Button(mansLogs, text="4", padx="40",bd=10 , pady="20", command=lambda:btnClick(4))
+btn5=Button(mansLogs, text="5", padx="40",bd=10 , pady="20", command=lambda:btnClick(5))
+btn6=Button(mansLogs, text="6", padx="40",bd=10 , pady="20", command=lambda:btnClick(6))
+btn7=Button(mansLogs, text="7", padx="40",bd=10 , pady="20", command=lambda:btnClick(7))
+btn8=Button(mansLogs, text="8", padx="40",bd=10 , pady="20", command=lambda:btnClick(8))
+btn9=Button(mansLogs, text="9", padx="40",bd=10 , pady="20", command=lambda:btnClick(9))
 
-btnnodzes=Button(mansLogs, text="C", padx="40", pady="20", command=Clear)
-btnvienadiba=Button(mansLogs, text="=", padx="40", pady="20", command=btnEquals)
+btnnodzes=Button(mansLogs, text="C", padx="40",bd=10 , pady="20", command=Clear)
+btnvienadiba=Button(mansLogs, text="=", padx="40",bd=10 , pady="20", command=btnEquals)
 
-btnminus=Button(mansLogs, text="-", padx="40", pady="20", command=lambda:btnCommand("-"))
-btnpluss=Button(mansLogs, text="+", padx="40", pady="20", command=lambda:btnCommand("+"))
-btnreiz=Button(mansLogs, text="x", padx="40", pady="20", command=lambda:btnCommand("*"))
-btndev=Button(mansLogs, text="/", padx="40", pady="20", command=lambda:btnCommand("/"))
-#btn=Button(mansLogs, text="9", padx="40", pady="20")
+btnminus=Button(mansLogs, text="-", padx="40",bd=10 , pady="20", command=lambda:btnCommand("-"))
+btnpluss=Button(mansLogs, text="+", padx="40", bd=10 ,pady="20", command=lambda:btnCommand("+"))
+btnreiz=Button(mansLogs, text="x", padx="40", bd=10 ,pady="20", command=lambda:btnCommand("*"))
+btndev=Button(mansLogs, text="/", padx="40", bd=10 ,pady="20", command=lambda:btnCommand("/"))
+btnprocent=Button(mansLogs, text="%", padx="40", bd=10 ,pady="20", command=lambda:btnCommand("%"))
+btnsqrt=Button(mansLogs, text="_/", padx="40", bd=10 ,pady="20", command=lambda:btnCommand("_/"))
+
 
 btn1.grid(row=1,column=1)
 btn2.grid(row=1,column=2)
@@ -81,6 +99,9 @@ btnpluss.grid(row=1,column=4)
 btnminus.grid(row=2,column=4)
 btnreiz.grid(row=3,column=4)
 btndev.grid(row=4,column=4)
+
+btnprocent.grid(row=1,column=6)
+btnsqrt.grid(row=2,column=6)
 
 e.grid(row=0, column=1, columnspan=4)
 
